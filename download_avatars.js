bee57fcb2c37ddf997e16f05cb67402f1ab648ca
Song Gao <song.d.gao@gmail.com>
2018-01-23 06:03:20 +0000

    added the callback function

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,38 @@
 request = require('request');
+fs = require('fs');
 token = require('./secrets');
+var repo = process.argv.slice(2);
+
+if (repo.length !== 2) {
+  return console.log("I can't continue without the repo owner and repo names!");
+}
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// download from URL and save
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('err', err => { throw err; })
+    .pipe(fs.createWriteStream(filePath));
+}
+
+// loop through all contributors to save their avatar_url
+function saveAvatar(err, body) {
+  if (err) throw err;
+
+  JSON.parse(body).forEach(user => {
+    downloadImageByURL(user.avatar_url, ('./avatars/' + user.login + '.png'))
+  })
+}
+
+// find contributors info from a repo
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request'
-    }
+    },
+    token: token.GITHUB_TOKEN
   };
 
   request(options, function(err, res, body) {
@@ -16,8 +40,4 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-
-});
+getRepoContributors(repo[0], repo[1], saveAvatar);
